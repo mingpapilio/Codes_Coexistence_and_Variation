3101a6f1d51d4b16b6bee4451cd6e935da23a9d2
mingpapilio <39230189+mingpapilio@users.noreply.github.com>
2021-07-01 02:53:54 +0100

    Delete data directory

--- data/Fig3/tseries.R
+++
@@ -1,7 +0,0 @@
-## Plot the time series
-raw<-read.csv("out.txt",sep="\t")
-dev.new()
-par(mar=c(5,4,2,2))
-plot(raw[,3],raw[,1],type="l",lwd=2,col="orange",xlab="Time",ylab="Population size",
-     ylim=c(0,10000))
-points(raw[,3],raw[,2],type="l",lwd=2,col="skyblue")
